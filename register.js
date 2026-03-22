f95fc31898e14c9a3462ab857f79e46d3d0c62ae
takaqiao <532236728@qq.com>
2026-03-22 08:28:28 +0800

    fix: stabilize babele table translation and release v1.0.2

--- register.js
+++ register.js
@@ -22,6 +22,21 @@ function safeTableResultsCollection(collection, translations) {
 }
 
 Hooks.once('babele.init', (babele) => {
+  // Guard against malformed RollTable result translations that can crash
+  // Babele's internal _tableResults converter on some adventure entries.
+  const internalConverters = babele?.converters;
+  const originalTableResults = internalConverters?._tableResults;
+  if (typeof originalTableResults === 'function') {
+    internalConverters._tableResults = function patchedTableResults(collection, translations, ...args) {
+      try {
+        return originalTableResults.call(this, collection, translations, ...args);
+      } catch (error) {
+        console.warn(`${MODULE_ID} | Falling back from Babele _tableResults converter`, error);
+        return collection;
+      }
+    };
+  }
+
   babele.registerConverters({
     safeTableResultsCollection,
   });
